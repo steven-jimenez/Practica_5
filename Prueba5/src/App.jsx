bd5ab3fec1f24003388701dd1d2ae8ed0f9b6fd3
Steven Jimenez <stevenjimenez00@hotmail.com>
2023-06-16 17:02:57 -0400

    se agregaron las imagenes

--- Prueba5/src/App.jsx
+++ Prueba5/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <>
       <h1>Practica #5</h1>
+
+      <div className="imagenes">
+        <img src="https://sm.ign.com/t/ign_latam/screenshot/default/patooward_y5xr.1280.jpg" alt="Ellie" />
+        <img src="https://www.martyfriedel.com/img/asset/YXNzZXRzL2dhbWVzL3RoZS1sYXN0LW9mLXVzLXBhcnQtaWkvam9lbC5qcGc=?w=1024&fit=max&dpr=2&q=75&s=0eaf89006fe5966e035ff94dc571c3d4" alt="Joel" />
+        <img src="https://1.bp.blogspot.com/-KVglc2G_WwA/XzAEYKwJHzI/AAAAAAAALdw/fz7lRJ-bm4ctlAdf3HBp7r5e7ffG7pRHgCNcBGAsYHQ/s1600/frank-tzeng-frank-tzeng-abby-t1-headter.jpg" alt="Abby" />
+      </div>
     </>
   )
 }
